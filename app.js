11f6ffafd376a871361b18d263be26fa3579b3cd
lymanjohnson <lyman.johnson@gmail.com>
2017-08-14 15:00:04 -0400

    Variables all sorted out

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
 const moment = require('moment');
 const chalk = require('chalk');
+
+// console.log(chalk.blue('Hello') + 'World' + chalk.red('!'));
+let time = moment();
+let year = time.years();
+let month = time.months();
+let day = time.days();
+let dayName = time.weekday();
+let hour = time.hours();
+let minute = time.minutes();
+let second = time.seconds();
+let leapyear = time.isLeapYear();
+
+console.log(year);
+console.log(month);
+console.log(day);
+console.log(dayName);
+console.log(hour);
+console.log(minute);
+console.log(second);
